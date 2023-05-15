239a30584a5ceae2ea90cd62b5ac4be6ae7b44f6
Drwalin <marek.zalewski.drwalin@gmail.com>
2023-05-15 14:48:21 +0200

    Empty files for third etap.

---
+++ unoptimised_queries/select_all_entities_during_time_in_location_during_time_frame.sql
@@ -0,0 +1,34 @@
+
+-- select all entities that were in location during given time frame
+
+CREATE OR REPLACE TYPE t_ints AS TABLE OF INT;
+CREATE OR REPLACE TYPE t_stringa AS TABLE OF VARCHAR2(64);
+
+
+CREATE OR REPLACE FUNCTION SelectAllEntitiesInLocationDuring(
+		locationn IN VARCHAR2,
+		timeStart IN TIMESTAMP,
+		timeEnd IN TIMESTAMP
+) RETURN t_strings AS
+	ret t_strings;
+BEGIN
+	SELECT name BULK COLLECT INTO ret
+		FROM items I1
+		WHERE
+		WHERE E1.location = locationn AND E1.date_time <= timepoint
+		AND
+		(SELECT count(*) FROM entities_entered_location E2
+			WHERE E2.location = locationn
+			AND E2.name = E1.name
+			AND E2.date_time > E1.date_time
+			AND E2.date_time <= timepoint
+		) = 0
+		GROUP BY E1.name;
+	RETURN ret;
+END;
+
+
+
+
+
+
