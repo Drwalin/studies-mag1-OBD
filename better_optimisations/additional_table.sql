66d366a1cda47b1481fe0f27f3e1562f40671a3d
Drwalin <marek.zalewski.drwalin@gmail.com>
2023-06-28 22:50:40 +0200

    Added optimised items_of_player_int_timepoint

--- better_optimisations/additional_table.sql
+++ better_optimisations/additional_table.sql
@@ -8,8 +8,6 @@ CREATE OR REPLACE VIEW basic_entity_item_receivings AS
 	WHERE T1.id = TE1.transaction;
 
 INSERT INTO entity_item_receivings (stamp, item, owner, transaction, abandonmentTIme)
-SELECT * FROM (
-	SELECT * FROM (
 		SELECT b1.stamp, b1.item, b1.owner, b1.transaction, b2.stamp
 		FROM basic_entity_item_receivings b1, basic_entity_item_receivings b2
 		WHERE b1.item = b2.item
@@ -21,9 +19,7 @@ SELECT * FROM (
 				AND b3.stamp < b2.stamp
 				AND b3.stamp > b1.stamp
 			) = 0
-		)
 	UNION
-	SELECT * FROM (
 		SELECT b1.stamp, b1.item, b1.owner, b1.transaction, NULL
 		FROM basic_entity_item_receivings b1
 		WHERE (
@@ -31,52 +27,9 @@ SELECT * FROM (
 				FROM basic_entity_item_receivings b3
 				WHERE b3.item = b1.item
 				AND b3.stamp > b1.stamp
-			) = 0
-		)
-	);
+			) = 0;
 	
 DELETE entity_item_receivings;
-
--- DECLARE
--- 	CURSOR en IS
--- 		SELECT e1.stamp, e1.item, e1.owner, e2.stamp
--- 		FROM entity_item_receivings e1, entity_item_receivings e2
--- 		WHERE e1.item = e2.item
--- 		AND e2.stamp > e1.stamp
--- 		AND (SELECT COUNT(*)
--- 			FROM entity_item_receivings e3
--- 			WHERE e1.stamp < e3.stamp
--- 			AND e2.stamp > e3.stamp
--- 		) = 0;
--- 	stamp TIMESTAMP;
--- 	item INT;
--- 	owner VARCHAR(64);
--- 	stamp2 TIMESTAMP;
--- BEGIN
--- 	DELETE entity_item_receivings;
--- 	
--- 	INSERT INTO entity_item_receivings (stamp, item, owner, transaction)
--- 		SELECT
--- 			T1.stamp,
--- 			TE1.item, 
--- 			CASE WHEN TE1.from_a = 0 THEN T1.owner_a ELSE T1.owner_b END,
--- 			T1.id
--- 		FROM transaction_entries TE1, transactions T1
--- 		WHERE T1.id = TE1.transaction;
--- 	
--- 	OPEN en;
--- 	LOOP
--- 		FETCH en INTO stamp, item, owner, stamp2;
--- 		EXIT WHEN en%NOTFOUND;
--- 
--- 		UPDATE entity_item_receivings e1
--- 			SET abandonmentTIme=stamp2
--- 			WHERE e1.stamp = stamp
--- 			AND e1.owner = owner
--- 			AND e1.item = item;
--- 	END LOOP;
--- 	CLOSE en;
--- END;
 
 
 DROP TABLE entity_item_receivings;
--- better_optimisations/items_of_player_in_timepoint.sql
+++ better_optimisations/items_of_player_in_timepoint.sql
@@ -4,26 +4,25 @@ CREATE OR REPLACE FUNCTION SelectItemsOfPlayerInTimepointOptimised(
 ) RETURN VARCHAR2 SQL_MACRO AS
 BEGIN
 	RETURN q'{
-	SELECT DISTINCT TE1.id, TE1.TRANSACTION, TE1.item,
-			CASE WHEN TE1.from_a = 1 THEN T1.owner_a ELSE T1.owner_b END giver,
-			CASE WHEN TE1.from_a = 0 THEN T1.owner_a ELSE T1.owner_b END taker,
-			T1.stamp, TE1.from_a, T1.owner_a, T1.owner_b
-		FROM transaction_entries TE1, transactions T1
-		WHERE TE1.transaction = T1.id
-		AND T1.stamp <= timepoint
-		AND ((
-			TE1.from_a = 1
-			AND playerName = T1.owner_b
-		) OR (
-			TE1.from_a = 0
-			AND playerName = T1.owner_a
-		))
-		AND (SELECT COUNT(*) FROM transaction_entries TE2, transactions T2
-			WHERE TE2.transaction = T2.id
-			AND TE2.item = TE1.item
-			AND T2.stamp <= timepoint
-			AND T2.stamp > T1.stamp
-		) = 0
+	SELECT * FROM entity_item_receivings
+		WHERE stamp <= timepoint
+		AND (
+			abandonmentTIme >= timepoint
+			OR abandonmentTIme IS NULL
+		)
 	}';
 END;
 
+SELECT * FROM SYS.USER_ERRORS;
+
+SELECT item FROM SelectItemsOfPlayerInTimepointOptimised('Jamie',
+	TO_TIMESTAMP('2005-02-02 12:12:12.000', 'YYYY-MM-DD HH24:MI:SS.FF6')) ORDER BY item;
+
+SELECT item FROM SelectItemsOfPlayerInTimepoint('Jamie',
+	TO_TIMESTAMP('2005-02-02 12:12:12.000', 'YYYY-MM-DD HH24:MI:SS.FF6')) ORDER BY item;
+
+SELECT id FROM SelectItemsOfPlayerInTimepointUnoptimisedVery('Jamie',
+	TO_TIMESTAMP('2005-02-02 12:12:12.000', 'YYYY-MM-DD HH24:MI:SS.FF6')) ORDER BY id;
+
+SELECT id FROM SelectItemsOfPlayerInTimepointUnoptimisedVery2('Jamie',
+	TO_TIMESTAMP('2005-02-02 12:12:12.000', 'YYYY-MM-DD HH24:MI:SS.FF6')) ORDER BY id;
