0e72ccb0737b2b18d8f3341b320a52cec40101c2
Drwalin <marek.zalewski.drwalin@gmail.com>
2023-06-30 00:46:33 +0200

    4 queries completely done.

--- better_optimisations/items_in_location_in_timepoint.sql
+++ better_optimisations/items_in_location_in_timepoint.sql
@@ -1,3 +1,15 @@
+CREATE OR REPLACE FUNCTION SelectAllItemsInLocationInTimepointOptimised0(
+		location IN VARCHAR2,
+		timepoint IN TIMESTAMP
+) RETURN VARCHAR2 SQL_MACRO AS
+BEGIN
+	RETURN q'{
+	SELECT I1.item
+		FROM SelectAllEntitiesInLocationInTimepointOptimised(location, timepoint) E1,
+			SelectItemsOfPlayerInTimepointOptimised(E1.name, timepoint) I1
+	}';
+END;
+
 CREATE OR REPLACE FUNCTION SelectAllItemsInLocationInTimepointOptimised1(
 		location IN VARCHAR2,
 		timepoint IN TIMESTAMP
@@ -41,6 +53,9 @@ SELECT item FROM SelectAllItemsInLocationInTimepointOptimised2('Alabama',
 		TO_TIMESTAMP('2000-01-15 15:14:12.000', 'YYYY-MM-DD HH24:MI:SS.FF6')) ORDER BY item;
 
 SELECT item FROM SelectAllItemsInLocationInTimepointOptimised1('Alabama',
+		TO_TIMESTAMP('2000-01-15 15:14:12.000', 'YYYY-MM-DD HH24:MI:SS.FF6')) ORDER BY item;
+
+SELECT item FROM SelectAllItemsInLocationInTimepointOptimised0('Alabama',
 		TO_TIMESTAMP('2000-01-15 15:14:12.000', 'YYYY-MM-DD HH24:MI:SS.FF6')) ORDER BY item;
 
 SELECT item FROM SelectAllItemsInLocationInTimepoint('Alabama',
